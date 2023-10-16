b4a49348a7deb1a7826a62f6a2f034257bdd3e88
Maxim Chadaev <mchadaev@unc.edu>
2023-10-15 21:23:15 -0400

    user input loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Test env for python."""
+
 from elevenlabs import generate, stream, set_api_key
 import os
 import openai
@@ -10,14 +12,10 @@ set_api_key("")
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-      messages=[
-        {"role": "user", "content": "Who won the world series in 2020?  Elobrate with at least 5 sentances."}],
-    max_tokens=4000,
-    temperature=0,
-    stream=True
-)
+def get_user_input():
+    user_input = input("Enter a prompt: ")
+    return user_input
+  
 
 def response_to_token_stream(response):
     for chunk in response:
@@ -36,18 +34,26 @@ def token_to_sentence_stream(token_iter):
             yield s
             sentence = []
 
-audio_stream = generate(
-  #text=response_to_token_stream(response),
-  #text=tokenizer.sent_tokenize(response_to_token_stream(response)),
-  #text=nltk.sent_tokenize(response_to_token_stream(response)),
-  text=token_to_sentence_stream(response_to_token_stream(response)),
-  stream=True,
-  voice="Matilda",
-  model="eleven_multilingual_v2"
-)
 
-print("stream")
-stream(audio_stream)
+while True:
+    user_prompt = get_user_input()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": user_prompt}],
+        max_tokens=4000,
+        temperature=0,
+        stream=True
+    )
+
+    audio_stream = generate(
+        text=token_to_sentence_stream(response_to_token_stream(response)),
+        stream=True,
+        voice="Matilda",
+        model="eleven_multilingual_v2"
+    )
+
+    print("stream")
+    stream(audio_stream)
 
 # print(openai.Model.list())
-
